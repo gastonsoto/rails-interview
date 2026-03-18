217c999b00c61b42a3279d836b86dae6e1a1c4e4
Gastón Soto <code@gastonsoto.me>
2026-03-18 19:28:30 -0300

    fix: timestamps

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -4,6 +4,8 @@ class TodoList < ApplicationRecord
   has_many :bulk_operations, dependent: :destroy
 
   after_commit :broadcast_lists
+  before_create :set_timestamps_if_missing
+  before_update :set_timestamps_if_missing
 
   def self.broadcast_lists!(user)
     Turbo::StreamsChannel.broadcast_replace_to(
@@ -19,5 +21,11 @@ class TodoList < ApplicationRecord
 
   def broadcast_lists
     self.class.broadcast_lists!(user)
+  end
+
+  def set_timestamps_if_missing
+    now = Time.current
+    self.created_at ||= now if respond_to?(:created_at)
+    self.updated_at ||= now if respond_to?(:updated_at)
   end
 end
